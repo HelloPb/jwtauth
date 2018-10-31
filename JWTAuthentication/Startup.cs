9e814f075c591e89f26434271eb86d4c3ad383f4
Prasanna Bhat <prasanna.k.bhat@gmail.com>
2018-10-31 14:23:15 +1100

    intial

---
+++ JWTAuthentication/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace JWTAuthentication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "ROR Online - v1 API Specification",
+                    Description = "Describes the list of API's used for ROR Online project",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "ROR Online API", Email = "rorvic@outlook.com", Url = "rorvic.azurewebsites.net" }
+                });
+
+                c.SwaggerDoc("v2", new Info
+                {
+                    Version = "v2",
+                    Title = "ROR Online - v2 API Specification",
+                    Description = "Describes the list of API's used for ROR Online project",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "ROR Online API", Email = "rorvic@outlook.com", Url = "rorvic.azurewebsites.net" }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ROR Online - v1 API Specification");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "ROR Online - v2 API Specification");
+            });
+        }
+    }
+}
